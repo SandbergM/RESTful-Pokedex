a723594b20647b94536e229ac929f15bdb432dc3
Marcus Sandberg <marcus.a.sandberg@outlook.com>
2020-10-19 14:51:54 +0200

    Initial commit

---
+++ src/main/java/com/example/Pokedex/ResTfulPokedexApplication.java
@@ -0,0 +1,13 @@
+package com.example.Pokedex;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ResTfulPokedexApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ResTfulPokedexApplication.class, args);
+	}
+
+}
